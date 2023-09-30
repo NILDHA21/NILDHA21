560d01c986b8dc6baf0e0a8355d09dc6baa4a1aa
NILDHA21 <nileshdhage20@gmail.com>
2023-09-30 20:38:14 +0530

    basic.java

--- npd/src/npd/basic.java
+++ npd/src/npd/basic.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class basic {
 	public static void main(String[] args) {
 	int n = 5;	
+		System.out.println("Thank you");
 		for(int i=1; i<=n; i++) {
 			for(int j=1; j<=n-i; j++) {
 		       System.out.print(" ");
